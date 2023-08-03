4eb02b06c4d59b54fe71f9d85a8463bd4a07a74e
ManavJadhav <manavjdhv@gmail.com>
2023-08-04 03:39:19 +0530

    C_Programs added

---
+++ Program2/Main.c
@@ -0,0 +1,15 @@
+/////////////////////////////////////////////////////////////////
+//
+//  Display Jay Ganesh 5 Times On Screen.
+//
+//////////////////////////////////////////////////////////////////
+
+#include "Header.h"              // User defined header file inclusion
+
+// Entry point function
+int main()
+{
+  Display();                    // Function call
+       
+  return 0;                    // Return sucess to OS
+}
